bcb45534aa3822ca7f7d5fa9f24902a812d43198
William Kwako <wkwako3@gatech.edu>
2025-06-03 20:51:15 -0500

    reverting settings file

--- edtech_project/settings.py
+++ edtech_project/settings.py
@@ -26,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 with open('secret_key.txt', 'r') as file:
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = file.read()
-
-print (f'SECRET KEY IS: {SECRET_KEY}')
-
+    
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
